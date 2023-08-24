c8431158962b57623854187fbb9ea7acb79adcdb
Owen Harbert <owen.harbert2@gmail.com>
2023-08-24 22:40:54 +0200

    Added destructible interface

--- Assets/_Scripts/Ship/ShipFuel.cs
+++ Assets/_Scripts/Ship/ShipFuel.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class ShipFuel : ShipSystem
 {
-    
+    [SerializeField] private float fuelCapacity;
+    [SerializeField] private float currentFuelLevel;
+    [SerializeField] private float burnRate;
+
+    protected override void Awake() 
+    {
+        base.Awake();
+    }
+
+    private void OnEnable() 
+    {
+        
+    }
+
+    private void OnDisable() 
+    {
+        
+    }
 }
